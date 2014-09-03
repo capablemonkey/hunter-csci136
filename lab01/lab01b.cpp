0fc6c06e6410421cc0ef7bb1448b1704d63d5751
Gordon Zheng <technix1@gmail.com>
2014-09-02 20:18:22 -0400

    added first lab

---
+++ lab01/lab01b.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "1+3+5+7+9=" << 1+3+5+7+9 << endl;
+	return 0;
+}
---
+++ lab01/lab01c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+
+	cout << "Give me a number: ";
+	cin >> a;
+	cout << "Give me another one: "; 
+	cin >> b;
+
+	cout << "Sum: " << a + b << endl;
+	return 0;
+}
