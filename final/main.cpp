ea185713f35e6fc6252c34f1a1392a8fcd974e8f
Gordon Zheng <technix1@gmail.com>
2014-12-11 20:25:28 -0500

    convert indentation to spaces

--- final/main.cpp
+++ final/main.cpp
@@ -10,42 +10,42 @@
 using namespace std;
 
 struct Student {
-	string fullName;
-	int grade;
+  string fullName;
+  int grade;
 };
 
 double getAverageGrade(Student students[], int studentsCount) {
-	int sum = 0;
-	for (int i=0; i < studentsCount; i++) {
-		sum += students[i].grade;
-	}
+  int sum = 0;
+  for (int i=0; i < studentsCount; i++) {
+    sum += students[i].grade;
+  }
 
-	return (sum * 1.0) / studentsCount;
+  return (sum * 1.0) / studentsCount;
 }
 
 Student getBestStudent(Student students[], int studentsCount) {
-	int highestGrade = 0;
-	Student *highestStudent;
+  int highestGrade = 0;
+  Student *highestStudent;
 
-	for (int i=0; i < studentsCount; i++) {
-		if (students[i].grade > highestGrade) {
-			highestGrade = students[i].grade;
-			highestStudent = &students[i];
-		}
-	}
+  for (int i=0; i < studentsCount; i++) {
+    if (students[i].grade > highestGrade) {
+      highestGrade = students[i].grade;
+      highestStudent = &students[i];
+    }
+  }
 
-	return *highestStudent;
+  return *highestStudent;
 }
 
 int main(int argc, char *argv[]) {
-	// make sure arg is provided
-	if (argc < 2) { 
-		cerr << "Usage: ./grades <input_file>" << endl;
-		return 1;
-	}
+  // make sure arg is provided
+  if (argc < 2) { 
+    cerr << "Usage: ./grades <input_file>" << endl;
+    return 1;
+  }
 
-	// read file
-	ifstream inputFile;
+  // read file
+  ifstream inputFile;
   inputFile.open(argv[1], ios_base::in);
 
   // collect students:
@@ -59,13 +59,13 @@ int main(int argc, char *argv[]) {
 
   // parse file:
   while(inputFile) {
-  	inputFile >> firstName >> lastName >> grade;
-  	Student newStudent;
-  	newStudent.fullName = firstName + " " + lastName;
-  	newStudent.grade = grade;
+    inputFile >> firstName >> lastName >> grade;
+    Student newStudent;
+    newStudent.fullName = firstName + " " + lastName;
+    newStudent.grade = grade;
 
-  	students[studentsCount] = newStudent;
-  	studentsCount++;
+    students[studentsCount] = newStudent;
+    studentsCount++;
   }
 
   double averageGrade = getAverageGrade(students, studentsCount);
@@ -76,10 +76,10 @@ int main(int argc, char *argv[]) {
   cout.precision(2);
 
   cout << "In a class of " << studentsCount
-  		 << " students, the average grade was: " << averageGrade << endl
-  		 << "and the best student was " << bestStudent.fullName
-  		 << ", who scored " << bestStudent.grade
-  		 << "." << endl;
+       << " students, the average grade was: " << averageGrade << endl
+       << "and the best student was " << bestStudent.fullName
+       << ", who scored " << bestStudent.grade
+       << "." << endl;
   
   return 0;
 }
